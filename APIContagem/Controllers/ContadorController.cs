ded40f2545bbd320ae807c267c4fb13feb6b9168
Renato Groffe <renato.groffe@yahoo.com.br>
2020-10-03 08:35:44 -0300

    ASP.NET 5 Release Candidate 1 + REST API + Swagger + Docker + Linux
    ASP.NET 5 Release Candidate 1 + REST API + Swagger + Docker + Linux

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace APIContagem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContadorController : ControllerBase
+    {
+        private static readonly Contador _CONTADOR = new Contador();
+        private readonly ILogger<ContadorController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ContadorController(ILogger<ContadorController> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                _logger.LogInformation($"Contador - Valor atual: {_CONTADOR.ValorAtual}");
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    _CONTADOR.Local,
+                    _CONTADOR.Kernel,
+                    _CONTADOR.TargetFramework,
+                    MensagemFixa = "Teste",
+                    MensagemVariavel = _configuration["MensagemVariavel"]
+                };
+            }
+        }
+    }
+}
